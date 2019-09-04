eb388cc6fd236269f63468350b07a06bdee9b5aa
Tom Evans <tom.evans1664>
2019-09-04 15:42:16 +0100

    configure mongo db in memory

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const utils = require("./utils.js");
 
-app.get('/', async (req, res) => {
-    res.send('hello world');
+const { startDatabase } = require('./db/mongo');
+const { insertPalindrome, getPalindromes } = require('./controllers/palindrome');
+
+app.post('/palindromes/:string', async (req, res) => {
+    const string = req.params.string;
+    const isString = utils.isString(string);
+    if (!isString) return false;
+
+    const isPalindrome = utils.isPalindrome(string);
+    if (isPalindrome) await insertPalindrome({ text: string });
+    return res.send(isPalindrome)
 });
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.get('/palindromes', async (req, res) => {
+    res.send(await getPalindromes());
+});
+
+startDatabase().then(async () => {
+    app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+});
+
